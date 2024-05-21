d9083e123171ddb621a19c2b50c5a360586f6df2
spl-RerreRojas <renata.rojas07@gmail.com>
2024-05-20 22:13:00 -0400

    este siquesi

--- assets/js/cambiofecha.js
+++ assets/js/cambiofecha.js
@@ -1,9 +1,9 @@
-const cambiarOrdenFecha = fecha => {
-    return fecha.split('/').reverse().join('-');
-  };
-  
-  // Ejemplo de uso
-  const fechaOriginal = "2024/05/20";
-  const fechaNueva = cambiarOrdenFecha(fechaOriginal);
-  
-  console.log(fechaNueva); // "2023-12-25"
+const cambiarOrdenFecha = (fecha) => {
+  return fecha.split("/").reverse().join("-");
+};
+
+// Ejemplo de uso
+const fechaOriginal = "2024/05/20";
+const fechaNueva = cambiarOrdenFecha(fechaOriginal);
+
+console.log(fechaNueva); // "2023-12-25"
